3c30abb33c9819dca78f31c333dd059dc54bad29
MorisatoK <accounts@badlands-of-my-mind.de>
2015-11-01 12:39:28 +0100

    add spectrum colorpicker (temporary until there is a ng2 solution)

--- libs/typings/tsd.d.ts
+++ libs/typings/tsd.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="bootstrap/bootstrap.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="spectrum/spectrum.d.ts" />
+/// <reference path="tinycolor/tinycolor.d.ts" />
